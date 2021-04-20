91b01a6ab60337acaf464b96bf92206c214e98a2
Karan Newatia <kn348@cornell.edu>
2021-04-20 21:16:22 +0530

    vsr re construction

--- re_sharing/vsr.py
+++ re_sharing/vsr.py
@@ -1,14 +1,22 @@
 import random
+from lagrange import lagrange
 
 n = 10
 m = 6
-p = 97 #TODO change this to a 550 bit prime
+p = 11 #TODO change this to a 550 bit prime
+r = 23
 degree = 32768 #TODO do everthing 32768 times
 
 #TODO verification steps at both levels
 
+def modInverse(a, b):
+    for x in range(1, p):
+        if (((a%p) * (x%p)) % p == b):
+            return x
+    return -1
+
 k = random.randint(0, p-1)
-#print(k)
+print("original secret: ", k)
 a = [random.randint(0, p-1) for _ in range(m-1)]
 
 s_shares = []
@@ -19,49 +27,45 @@ for i in range(1, n+1):
         s_i = (s_i + ((((a[j-1]*i) % p)**j) % p)) % p
     #print(s_i)
     s_shares.append(s_i)
+print("shares: ", s_shares)
 
-generator = random.randint(0, p-1)
+print("original secret reconstructed: ", lagrange(s_shares, p))
+
+generator = 9 #random.randint(0, p-1)
 generator_lst = [0 for _ in range(m)]
-generator_lst[0] = (generator ** k) % p
+generator_lst[0] = (generator ** k) % r
 for i in range(1, m):
-    generator_lst[i] = (generator ** a[i-1]) % p
-#print(generator_lst)
+    generator_lst[i] = (generator ** a[i-1]) % r
+print("a vals: ", a)
+print("generator: ", generator)
+print("generator list: ", generator_lst)
+print(" ")
+for player in range(n):
+    print((generator ** s_shares[player]) % r)
+    prod = generator_lst[0]
+    for l in range(1, m):
+        #prod = (prod * (generator_lst[l] ** ((player+1)**l)) % p) % p
+        prod = (prod * (generator_lst[l] ** ((player+1)**l)) % r) % r
+    print(prod)
+    print(" ")
 
-# print((generator ** s_shares[0]) % p)
-# prod = generator_lst[0]
-# for j in range(1, m):
-#     prod = (prod * (generator_lst[j] ** 1)) % p
-# print(prod)
 
-
-sub_shares = [[0 for _ in range(n)] for _ in range(n)]
-for member in range(n): #TODO change the range to be the authorized subset
-    a_prime = [random.randint(0, p-1) for _ in range(m-1)]
-
-    for i in range(1, n+1):
-        s_i = s_shares[member]
-        for j in range(1, m):
-            s_i = (s_i + ((((a_prime[j-1]*i) % p)**j) % p)) % p
-        #print(s_i)
-        sub_shares[i-1][member] = s_i
-
-    generator_lst_new = [0 for _ in range(m)]
-    generator_lst_new[0] = (generator ** s_shares[member]) % p
-    for i in range(1, m):
-        generator_lst_new[i] = (generator ** a_prime[i-1]) % p
-
-# for i in range(n):
-#     print(sub_shares[i])
-
-new_shares = [0 for _ in range(n)]
-for j in range(n):
-    new_share = 0
-    for i in range(n): #TODO change the range to be the admissible subset
-        b = 1
-        for l in range(n): #TODO change the range to be the admissible subset
-            if l != i:
-                b *= l/(l-i)
-        new_share = (new_share + ((b * sub_shares[j][i]) % p)) % p
-    new_share = int(new_share)
-    new_shares[j] = new_share
-    #print(new_share)
+# sub_shares = [[0 for _ in range(n)] for _ in range(n)]
+# for member in range(n): #TODO change the range to be the authorized subset
+#     a_prime = [random.randint(0, p-1) for _ in range(m-1)]
+#
+#     for i in range(1, n+1):
+#         s_i = s_shares[member]
+#         for j in range(1, m):
+#             s_i = (s_i + ((((a_prime[j-1]*i) % p)**j) % p)) % p
+#         #print(s_i)
+#         sub_shares[i-1][member] = s_i
+#
+#     generator_lst_new = [0 for _ in range(m)]
+#     generator_lst_new[0] = (generator ** s_shares[member]) % p
+#     for i in range(1, m):
+#         generator_lst_new[i] = (generator ** a_prime[i-1]) % p
+#
+# new_shares = [lagrange(sub_shares[i], p) for i in range(n)]
+# print("new shares: ", new_shares)
+# print("secret reconstructed by new committee: ", lagrange(new_shares, p))
