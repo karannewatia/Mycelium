108bb98724f974e098ad3692820c6010f86b765e
Edo Roth <edoroth@gmail.com>
2021-05-04 12:42:24 -0400

    fixed graph

--- graph_scripts/mpc_success.py
+++ graph_scripts/mpc_success.py
@@ -18,31 +18,31 @@ plt.rc('font', **font)
 plt.gcf().subplots_adjust(bottom=0.25)
 plt.gcf().subplots_adjust(left=0.20)
 
-graph_a_1 = []
-graph_a_2 = []
-graph_a_3 = []
-
-byzantine_rates = []
-for i in [0.5,1,2,4]:
-    byzantine_rates.append(i)
-    result = graph_a(i/100, 5, 1)
-    graph_a_1.append(result)
-    result = graph_a(i/100, 10, 1)
-    graph_a_2.append(result)
-    result = graph_a(i/100, 15, 1)
-    graph_a_3.append(result)
-
-plt.plot(byzantine_rates, graph_a_1, marker="X", markersize=15, linewidth=5)
-plt.plot(byzantine_rates, graph_a_2, marker="X", markersize=15, linewidth=5)
-plt.plot(byzantine_rates, graph_a_3, marker="X", markersize=15, linewidth=5)
-plt.ylabel('Probability of privacy failure', fontsize='large')
-plt.xlabel('% of malicious users\n(a)', fontsize='large')
-plt.yscale('log')
-
-# plt.title('Probability of the MPC failing (committee size 10)')
-plt.legend(["C = 5", "C = 10", "C = 15"])
-plt.xticks(byzantine_rates, ('0.5', '1', '2', '4'))
-plt.savefig('../graphs/committee/mpc_failure_rate.pdf', format='pdf')
+#graph_a_1 = []
+#graph_a_2 = []
+#graph_a_3 = []
+#
+#byzantine_rates = []
+#for i in [0.5,1,2,4]:
+#    byzantine_rates.append(i)
+#    result = graph_a(i/100, 5, 1)
+#    graph_a_1.append(result)
+#    result = graph_a(i/100, 10, 1)
+#    graph_a_2.append(result)
+#    result = graph_a(i/100, 15, 1)
+#    graph_a_3.append(result)
+#
+#plt.plot(byzantine_rates, graph_a_1, marker="X", markersize=15, linewidth=5)
+#plt.plot(byzantine_rates, graph_a_2, marker="X", markersize=15, linewidth=5)
+#plt.plot(byzantine_rates, graph_a_3, marker="X", markersize=15, linewidth=5)
+#plt.ylabel('Probability of privacy failure', fontsize='large')
+#plt.xlabel('% of malicious users\n(a)', fontsize='large')
+#plt.yscale('log')
+#
+## plt.title('Probability of the MPC failing (committee size 10)')
+#plt.legend(["C = 5", "C = 10", "C = 15"])
+#plt.xticks(byzantine_rates, ('0.5', '1', '2', '4'))
+#plt.savefig('../graphs/committee/mpc_failure_rate.pdf', format='pdf')
 
 
 
@@ -65,7 +65,7 @@ plt.plot(x_vals, graph_b_3, marker="X", markersize=15, linewidth=5)
 #
 plt.ylabel('Probability of liveness', fontsize='large')
 plt.xlabel('% malice + churn\n(b)', fontsize='large')
-plt.title('Query succes rate with malice + churn (committee size 10)')
+#plt.title('Query succes rate with malice + churn (committee size 10)')
 plt.legend(["C = 5", "C = 10", "C = 15"])
 plt.xticks(x_vals, ('1', '2', '3', '4', '5', '6', '7'))
 #
