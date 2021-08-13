af15a94678620d6a8de41ed6433193733fd49cf7
Edo Roth <edoroth@gmail.com>
2021-08-13 11:28:24 -0400

    binary of mult

--- crypto/zokrates/multInput.py
+++ crypto/zokrates/multInput.py
@@ -3,10 +3,12 @@ import sys
 def gen_input(N):
 
     # Can set inputs to other things! These are arbitrary vectors
-    u = [1]*N
-    v = [2]*N
+    u = [1]*(10*N)
+    v = [1]*(10*N)
+    res_u = [1]*N
+    res_v = [1]*N
 
-    print(*u, *v)
+    print(*u, *v, *res_u, *res_v)
 
 gen_input(int(sys.argv[1]))
 
